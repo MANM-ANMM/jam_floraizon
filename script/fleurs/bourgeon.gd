9ac741598df211b20d7b14d6bb1370b1063ab69e
unknown <sml.gw.ld@gmail.com>
2023-12-16 17:26:01 +0100

    amélioration du bourgeon

--- script/fleurs/bourgeon.gd
+++ script/fleurs/bourgeon.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,11 +10,15 @@ func _ready():
 func _process(delta):
 	pass
 	
+	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("action_player") and has_overlapping_bodies():
 		var body = get_overlapping_bodies().front()
-		body.velocity.y-=800
+		var xo :Vector2= body.position - position
+		print("XO vaut:",xo)
+		print("XO normalisé vaut:",xo.normalized())
 		
+		body.velocity+=xo.normalized()*800
 
 func rappeler():
 	queue_free()
